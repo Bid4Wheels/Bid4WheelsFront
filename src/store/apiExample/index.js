73149b5ae41be136e84339ae2269eb9a2d9d49b7
tomasfreile <freiletomy@gmail.com>
2023-08-15 19:10:34 -0300

    added API

---
+++ src/store/apiExample/index.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const PAGE_SIZE = 12;
+
+// Esto es una API
+export const cardApiSlice = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://pokeapi.co/api/v2%22%7D)',
+        reducerPath: 'cardApi',
+        endpoints: (builder) => ({
+            getPokemonById: builder.query({
+                query: (id) => `pokemon/${id}`,
+            }),
+            getPokemonList: builder.query({
+                query: (page) => ({
+                    url: 'pokemon',
+                    params: { limit: PAGE_SIZE, offset: PAGE_SIZE * (page + 1) },
+                }),
+                serializeQueryArgs: ({ endpointName }) => {
+                    return endpointName;
+                },
+                merge: (currentCache, newItems) => {
+                    currentCache.push(...newItems);
+                },
+                transformResponse: (response) => response.results,
+                forceRefetch({ currentArg, previousArg }) {
+                    return currentArg !== previousArg;
+                },
+            }),
+        }),
+    }),
+});
+
+export const { useGetPokemonByIdQuery, useGetPokemonListQuery } = cardApiSlice;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counter/counterSlice';
+import { cardApiSlice } from './apiExample';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
+        [cardApiSlice.reducerPath]: cardApiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cardApiSlice.middleware),
 });
