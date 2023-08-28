a19aac7b1bd34ba19e1de1f0b367c62c2404b7a8
santi <santiago.peres@ing.austral.edu.ar>
2023-08-28 19:01:07 -0300

    changes: added token to userApi

--- src/store/user/userApi.js
+++ src/store/user/userApi.js
@@ -3,12 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const userApi = createApi({
     reducerPath: 'userApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.bid4wheels.com/',
+        baseUrl: 'https://api.bid4wheels.com/user',
+        prepareHeaders: (headers) => {
+            headers.set(
+                'authorization',
+                `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW51bWFzam9hbkBnbWFpbC5jb20iLCJpYXQiOjE2OTMyNTE5NjcsImV4cCI6MTY5Mzg1Njc2N30.4B0KPdII3_uj7hlA6IeU8mzSIo458fCuiS1j8oJR49c`,
+            );
+            return headers;
+        },
     }),
     endpoints: (builder) => ({
         updateUser: builder.mutation({
             query: (userId, userInfo) => ({
-                url: `PATCH /user/${userId}`,
+                url: `/${userId}`,
                 method: 'PATCH',
                 body: userInfo,
             }),
