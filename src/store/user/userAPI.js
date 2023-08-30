2bcb0b4948f2f5d5ec5f480bd8cc8b0c0c68a8f0
tomasfreile <freiletomy@gmail.com>
2023-08-29 22:33:16 -0300

    empty commit

--- src/store/user/userAPI.js
+++ src/store/user/userAPI.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { baseUrl } from '../../features/commons/Constants';
-//
+
 export const userApi = createApi({
     reducerPath: 'userApi',
     baseQuery: fetchBaseQuery({
