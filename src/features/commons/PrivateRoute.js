e0b707d10c052441d3ff261f93f5a7961cf2a10e
bautista massanti <bautista.massanti@ing.austral.edu.ar>
2023-09-04 16:00:22 -0300

    added Routes to PrivateRoute.js

--- src/features/commons/PrivateRoute.js
+++ src/features/commons/PrivateRoute.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { AuthenticatedUser } from './AuthenticatedUser';
 import Header from './Header';
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const PrivateRoute = ({ path, Component }) => {
     const loggedIn = AuthenticatedUser();
     return loggedIn ? (
         <>
             <Header />
-            <Route path={path} Component={Component} />
+            <Routes>
+                <Route path={path} Component={Component} />
+            </Routes>
         </>
     ) : (
         <Navigate to="/login" replace />
