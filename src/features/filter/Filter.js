b1e9f0dc1d8b7673fb5d95628e088aff87460082
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2023-09-05 19:13:15 -0300

    Fix:colors (except for Tags Autocomplete) + layout

--- src/features/filter/Filter.js
+++ src/features/filter/Filter.js
@@ -3,6 +3,7 @@ import {
     Autocomplete,
     Box,
     Button,
+    Checkbox,
     Chip,
     FormControlLabel,
     FormGroup,
@@ -10,11 +11,40 @@ import {
     TextField,
     Typography,
 } from '@mui/material';
-import { CheckBox } from '@mui/icons-material';
 import colors from '../../utils/desgin/Colors';
 import { BRANDS, COLORS, FUEL_TYPES, CAR_DOORS, GEAR_SHIFT_TYPES } from '../../utils/constants';
 
 export function Filter() {
+    const [selectedCarDoors, setSelectedCarDoors] = useState([]);
+    const [selectedGearShiftTypes, setSelectedGearShiftTypes] = useState([]);
+    const [selectedBrand, setSelectedBrand] = useState([]);
+    const [selectedColor, setSelectedColor] = useState([]);
+    const [selectedFuelType, setSelectedFuelType] = useState([]);
+    const [selectedModel, setSelectedModel] = useState([]);
+    const [selectedPriceMin, setSelectedPriceMin] = useState('');
+    const [selectedPriceMax, setSelectedPriceMax] = useState('');
+    const [selectedYearsMin, setSelectedYearsMin] = useState('');
+    const [selectedYearsMax, setSelectedYearsMax] = useState('');
+    const [selectedMileageMin, setSelectedMileageMin] = useState('');
+    const [selectedMileageMax, setSelectedMileageMax] = useState('');
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    const handleApplyFilters = () => {
+        console.log('Selected Car Doors:', selectedCarDoors);
+        console.log('Selected Gear Shift Types:', selectedGearShiftTypes);
+        console.log('Selected Brand:', selectedBrand);
+        console.log('Selected Color:', selectedColor);
+        console.log('Selected Fuel Type:', selectedFuelType);
+        console.log('Selected Model:', selectedModel);
+        console.log('Selected Price Min:', selectedPriceMin);
+        console.log('Selected Price Max:', selectedPriceMax);
+        console.log('Selected Years Min:', selectedYearsMin);
+        console.log('Selected Years Max:', selectedYearsMax);
+        console.log('Selected Mileage Min:', selectedMileageMin);
+        console.log('Selected Mileage Max:', selectedMileageMax);
+        console.log('Selected Tags:', selectedTags);
+    };
+
     return (
         <Grid
             container
@@ -28,9 +58,9 @@ export function Filter() {
             }}
         >
             <Grid item sx={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '20px' }}>
-                {customAutocomplete(BRANDS, 'Brand')}
-                {customAutocomplete(COLORS, 'Color')}
-                {customAutocomplete(FUEL_TYPES, 'Fuel Type')}
+                {customAutocomplete(BRANDS, 'Brand', setSelectedBrand)}
+                {customAutocomplete(COLORS, 'Color', setSelectedColor)}
+                {customAutocomplete(FUEL_TYPES, 'Fuel Type', setSelectedFuelType)}
             </Grid>
             <Grid
                 item
@@ -42,22 +72,43 @@ export function Filter() {
                     textAlign: 'center',
                 }}
             >
-                {customMinMaxField('Price')}
-                {customMinMaxField('Years')}
-                {customMinMaxField('Mileage')}
+                {customMinMaxField(
+                    'Price',
+                    selectedPriceMin,
+                    selectedPriceMax,
+                    setSelectedPriceMin,
+                    setSelectedPriceMax,
+                )}
+                {customMinMaxField(
+                    'Years',
+                    selectedYearsMin,
+                    selectedYearsMax,
+                    setSelectedYearsMin,
+                    setSelectedYearsMax,
+                )}
+                {customMinMaxField(
+                    'Mileage',
+                    selectedMileageMin,
+                    selectedMileageMax,
+                    setSelectedMileageMin,
+                    setSelectedMileageMax,
+                )}
                 <TextField
                     label="Model"
                     variant="standard"
-                    sx={{ width: '95%', maxWidth: '400px', height: '56px' }}
+                    color="water_green"
+                    sx={{ width: '95%', maxWidth: '400px', height: '56px', margin: 'auto' }}
+                    onChange={(event) => setSelectedModel(event.target.value)}
                 />
                 <Box justifyContent="center">
                     <Button
                         variant="contained"
+                        color="water_green"
                         sx={{
-                            backgroundColor: colors.water_green,
                             maxWidth: '100px',
                             width: '50%',
                         }}
+                        onClick={handleApplyFilters}
                     >
                         Apply
                     </Button>
@@ -73,49 +124,59 @@ export function Filter() {
                     paddingLeft: '20px',
                 }}
             >
-                <Autocomplete
-                    multiple
-                    freeSolo
-                    sx={{ width: '95%', maxWidth: '400px', height: '56px' }}
-                    id="tags-standard"
-                    options={top100Films.map((option) => option.title)}
-                    renderTags={(value, getTagProps) =>
-                        value.map((option, index) => (
-                            <Chip
-                                variant="outlined"
-                                label={option}
-                                key={index}
-                                {...getTagProps({ index })}
-                            />
-                        ))
-                    }
-                    renderInput={(params) => (
-                        <TextField {...params} variant="standard" label="Tags" placeholder="Tag" />
-                    )}
-                />
-                {customCheckBoxMapping('Car Doors', CAR_DOORS)}
-                {customCheckBoxMapping('Gear Shift Type', GEAR_SHIFT_TYPES)}
+                <Box style={{ maxHeight: '250px', overflowY: 'auto' }}>
+                    {customTagsAutocomplete(selectedTags, setSelectedTags)}
+                </Box>
+                {customCheckBoxMapping(
+                    'Car Doors',
+                    CAR_DOORS,
+                    selectedCarDoors,
+                    setSelectedCarDoors,
+                )}
+                {customCheckBoxMapping(
+                    'Gear Shift Type',
+                    GEAR_SHIFT_TYPES,
+                    selectedGearShiftTypes,
+                    setSelectedGearShiftTypes,
+                )}
             </Grid>
         </Grid>
     );
 }
 
-function customAutocomplete(opts, label) {
+function customAutocomplete(opts, label, setConstant) {
+    const handleItemChange = (value) => {
+        setConstant(value);
+    };
+
     return (
         <Autocomplete
             disablePortal
             id="combo-box-demo-brand"
             options={opts}
+            color="water_green"
             sx={{
                 width: '95%',
+                maxWidth: '400px',
                 height: '56px',
+                marginX: 'auto',
+                '&.Mui-focused .MuiInputLabel-outlined': {
+                    color: colors.water_green,
+                },
+                '& .MuiOutlinedInput-notchedOutline': {
+                    borderColor: colors.water_green,
+                },
+                '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                    borderColor: colors.water_green,
+                },
             }}
             renderInput={(params) => <TextField {...params} label={label} />}
+            onChange={(event, value) => handleItemChange(value)}
         />
     );
 }
 
-function customMinMaxField(fieldName) {
+function customMinMaxField(fieldName, min, max, setMin, setMax) {
     return (
         <Grid
             container
@@ -138,7 +199,10 @@ function customMinMaxField(fieldName) {
                         variant="standard"
                         inputProps={{ maxLength: 10 }}
                         fullWidth
+                        color="water_green"
                         style={{ marginRight: '8px' }}
+                        value={min}
+                        onChange={(event) => setMin(event.target.value)}
                     />
                     <span>-</span>
                     <TextField
@@ -146,7 +210,10 @@ function customMinMaxField(fieldName) {
                         variant="standard"
                         inputProps={{ maxLength: 10 }}
                         fullWidth
+                        color="water_green"
                         style={{ marginLeft: '8px' }}
+                        value={max}
+                        onChange={(event) => setMax(event.target.value)}
                     />
                 </Box>
             </Grid>
@@ -154,16 +221,64 @@ function customMinMaxField(fieldName) {
     );
 }
 
-function customCheckBoxMapping(title, opts) {
+function customTagsAutocomplete(selectedTags, setSelectedTags) {
+    return (
+        <Autocomplete
+            multiple
+            freeSolo
+            color="water_green"
+            sx={{
+                width: '95%',
+                maxWidth: '400px',
+                height: '56px',
+                marginX: 'auto',
+                '&.Mui-focused .MuiInputLabel-root': {
+                    color: colors.water_green,
+                },
+                '& .MuiFormControl-root': {
+                    color: colors.water_green,
+                },
+            }}
+            id="tags-standard"
+            options={top100Films.map((option) => option.title)}
+            value={selectedTags}
+            onChange={(event, value) => setSelectedTags(value)}
+            renderTags={(value, getTagProps) =>
+                value.map((option, index) => (
+                    <Chip
+                        variant="outlined"
+                        label={option}
+                        key={index}
+                        {...getTagProps({ index })}
+                    />
+                ))
+            }
+            renderInput={(params) => (
+                <TextField {...params} variant="standard" label="Tags" placeholder="Tag" />
+            )}
+        />
+    );
+}
+
+function customCheckBoxMapping(title, opts, selectedValues, setSelectedValues) {
+    const handleCheckBoxChange = (value) => {
+        let updatedValues;
+        if (selectedValues.includes(value)) {
+            updatedValues = [];
+        } else {
+            updatedValues = [value];
+        }
+        setSelectedValues(updatedValues);
+    };
+
     return (
         <Grid
             container
             sx={{
                 display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
                 width: '95%',
-                height: '56px',
                 justifyContent: 'center',
             }}
         >
@@ -172,13 +287,22 @@ function customCheckBoxMapping(title, opts) {
                 sx={{
                     display: 'flex',
                     flexDirection: 'row',
-                    gap: '15px',
+                    gap: '10px',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    margin: 'auto',
                 }}
             >
                 {opts.map((opt) => (
                     <FormControlLabel
-                        control={<CheckBox color={colors.water_green} />}
                         key={opt}
+                        control={
+                            <Checkbox
+                                color="water_green"
+                                checked={selectedValues.includes(opt)}
+                                onChange={() => handleCheckBoxChange(opt)}
+                            />
+                        }
                         label={opt}
                     />
                 ))}
