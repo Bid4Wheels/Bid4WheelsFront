5f0e4f45d4789ab5a1655c09df9e019320740dfc
valen <valentino.dicapua@ing.austral.edu.ar>
2023-09-07 20:31:48 -0300

    Fixed private routes bug

--- src/features/commons/PrivateRoute.js
+++ src/features/commons/PrivateRoute.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from './Header';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export const PrivateRoute = ({ children }) => {
     const loggedIn = !!useSelector((state) => state.user.token);
-    if (!children) return loggedIn ? <Header /> : <Navigate to="/login" replace />;
-    return loggedIn ? (
-        <>
-            <Header />
-            {children}
-        </>
-    ) : (
-        <Navigate to="/login" replace />
-    );
+    if (loggedIn) {
+        return (
+            <>
+                <Header />
+                {children}
+            </>
+        );
+    } else {
+        return <Navigate to={'/login'} replace />;
+    }
 };
