7cc1ccb8559da0acd45f3387001019e0a7cf4609
tomasfreile <freiletomy@gmail.com>
2023-09-11 16:42:43 -0300

    modified auctionApi

--- src/store/auction/auctionApi.js
+++ src/store/auction/auctionApi.js
@@ -12,7 +12,32 @@ export const auctionApi = createApi({
                 method: 'GET',
             }),
         }),
+        getAuctionList: builder.query({
+            query: (page, size) => ({ url: '/', params: { page, size } }),
+            forceRefetch({ currentArg, previousArg }) {
+                return currentArg !== previousArg;
+            },
+            serializeQueryArgs: ({ endpointName }) => {
+                return endpointName;
+            },
+            merge: (currentCache, newItems) => {
+                currentCache.push(...newItems);
+            },
+        }),
+        getFilteredAuctions: builder.query({
+            query: (page, size, filter) => ({ url: '/filter', params: { page, size, filter } }),
+            forceRefetch({ currentArg, previousArg }) {
+                return currentArg !== previousArg;
+            },
+            serializeQueryArgs: ({ endpointName }) => {
+                return endpointName;
+            },
+            merge: (currentCache, newItems) => {
+                currentCache.push(...newItems);
+            },
+        }),
     }),
 });
 
-export const { useGetAuctionByIdQuery } = auctionApi;
+export const { useGetAuctionByIdQuery, useGetFilteredAuctionsQuery, useGetAuctionListQuery } =
+    auctionApi;
