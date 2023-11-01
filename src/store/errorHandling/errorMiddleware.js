fa56fade25fa383ad05c4889e13ca2df7ff38328
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2023-11-01 09:53:44 -0300

    fix: deleted console logs

--- src/store/errorHandling/errorMiddleware.js
+++ src/store/errorHandling/errorMiddleware.js
@@ -1,14 +1,8 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit';
 import { addError } from './errorSlice';
 
-/**
- * Log a warning and show a toast!
- */
 export const rtkQueryErrorLogger = (store) => (next) => (action) => {
-    // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
     if (isRejectedWithValue(action)) {
-        console.warn('We got a rejected action!');
-        console.warn(action.payload.data);
         store.dispatch(addError(action.payload.data));
     }
 
