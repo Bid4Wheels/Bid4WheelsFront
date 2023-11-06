bfcad9a6a0770906ca1c5476162a69230b4ec553
tomasfreile <freiletomy@gmail.com>
2023-11-06 09:46:21 -0300

    feat: added sliding animation to images

--- src/features/auction/ImageCarousel.js
+++ src/features/auction/ImageCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, IconButton } from '@mui/material';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -6,13 +6,28 @@ import colors from '../../utils/desgin/Colors';
 
 export function ImageCarousel({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [nextIndex, setNextIndex] = useState(0);
+    const [isAnimating, setIsAnimating] = useState(false);
+    const [direction, setDirection] = useState(1);
+
+    useEffect(() => {
+        if (nextIndex !== currentIndex) {
+            setIsAnimating(true);
+            setDirection(nextIndex > currentIndex ? 1 : -1);
+            const timeoutId = setTimeout(() => {
+                setCurrentIndex(nextIndex);
+                setIsAnimating(false);
+            }, 500);
+            return () => clearTimeout(timeoutId);
+        }
+    }, [nextIndex, currentIndex]);
 
     const handlePrevClick = () => {
-        setCurrentIndex((prevIndex) => Math.max(prevIndex - 2, 0));
+        setNextIndex(Math.max(currentIndex - 2, 0));
     };
 
     const handleNextClick = () => {
-        setCurrentIndex((prevIndex) => Math.min(prevIndex + 2, images.length - 2));
+        setNextIndex(Math.min(currentIndex + 2, images.length - 2));
     };
 
     if (!images || images.length === 0) {
@@ -58,7 +73,7 @@ export function ImageCarousel({ images }) {
         >
             <IconButton
                 onClick={handlePrevClick}
-                disabled={currentIndex === 0}
+                disabled={isAnimating || currentIndex === 0}
                 sx={{
                     backgroundColor: colors.water_green,
                     borderRadius: '50%',
@@ -69,7 +84,14 @@ export function ImageCarousel({ images }) {
                 <NavigateBeforeIcon />
             </IconButton>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    transform: `translateX(${direction * (currentIndex - nextIndex) * 50}%)`,
+                    transition: 'transform 0.2s ease-in-out',
+                }}
+            >
                 {images.slice(currentIndex, currentIndex + 2).map((image, index) => (
                     <div
                         key={index}
@@ -78,6 +100,8 @@ export function ImageCarousel({ images }) {
                             height: '260px',
                             width: '23vw',
                             flexBasis: 'calc(50% - 20px)',
+                            opacity: isAnimating ? 0 : 1,
+                            transition: 'opacity 0.5s ease-in-out',
                         }}
                     >
                         <img
@@ -98,7 +122,7 @@ export function ImageCarousel({ images }) {
 
             <IconButton
                 onClick={handleNextClick}
-                disabled={currentIndex >= images.length - 2}
+                disabled={isAnimating || currentIndex >= images.length - 2}
                 sx={{
                     backgroundColor: colors.water_green,
                     borderRadius: '50%',
